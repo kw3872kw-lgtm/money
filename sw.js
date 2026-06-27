a15930541d51ec80ede7e8bcf8f08e3c0db74316
kw3872kw-lgtm <kw3872kw@gmail.com>
2026-06-27 14:51:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-const CACHE = 'meter-v4';
+const CACHE = 'meter-v5';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html','./manifest.json','./sw.js'])));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
+    '/money/index.html',
+    '/money/manifest.json',
+    '/money/sw.js'
+  ])));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html'))));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/money/index.html')))
+  );
 });
